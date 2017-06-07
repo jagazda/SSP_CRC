4f93c9c366acfc6ca3728f1fa7b0b3cd05a20df7
creex9 <gpcreex@gmail.com>
2017-06-07 23:35:59 +0200

    Added signals descriptions

--- AHB_MASTER.v
+++ AHB_MASTER.v
@@ -2,40 +2,44 @@ module AHB_MAST #(parameter [31:0] DATA_WIDTH = 32'd32,ADDR_WIDTH = 32'd32)(
 
     //GENERAL SIGNALS
     input wire HCLK,
-    input wire RESET,
+    input wire RESET,                       // Active LOW
     //AMBA AHB SIGNALS
     //Signals from SLAVE
-    input wire [DATA_WIDTH-1:0] HRDATA,
-    input wire HREADY,
-    input wire HRESP,
+    input wire [DATA_WIDTH-1:0] HRDATA,     // DATA form Slave to master
+    input wire HREADY,                      // When HIGH-transfer finished, LOW to extend transfer
+    input wire HRESP,                       // Transfer status : LOW- OKAY, HIGH-ERROR
 
     //MASTER OUT
-    output reg [ADDR_WIDTH-1:0] HADDR,
-    output reg [DATA_WIDTH-1:0] HWDATA,
-    output reg [2:0] HBURST,
-    output reg [2:0] HSIZE,
-    output reg [1:0] HTRANS,
-    output reg HWRITE,
-    output reg HMASTLOCK,
-    output reg HSEL,
+    output reg [ADDR_WIDTH-1:0] HADDR,      // ADDR to slave
+    output reg [DATA_WIDTH-1:0] HWDATA,     // DATA form Master to Slave
+    output reg [2:0] HBURST,                // BURST type : single incr wrap
+    output reg [2:0] HSIZE,                 // SIZE of transfer : byte, halfword or word - max 1024bits
+    output reg [1:0] HTRANS,                // Transfer type:  IDLE,BUSY,NONSEQ,SEQ
+    output reg HWRITE,                      // Transfer direction: HIGH-write, LOW-read
+    output reg HMASTLOCK,                   // HIGH when locked sequence
+    output reg HSEL,                        // Unnecessary, here only one slave
     
 );
     //HTRANS TYPES
-    localparam IDLE     = 2'b00;
-    localparam BUSY     = 2'b01;
-    localparam NONSEQ   = 2'b10;
-    localparam SEQ      = 2'b11;
+    localparam IDLE             = 2'b00;
+    localparam BUSY             = 2'b01;
+    localparam NONSEQ           = 2'b10;
+    localparam SEQ              = 2'b11;
 
     //HBURST TYPES
-    localparam SINGLE   = 3'b000;
-    localparam INCR     = 3'b001;
-    localparam WRAP4    = 3'b010;
-    localparam INCR4    = 3'b011;
-    localparam WRAP8    = 3'b100;
-    localparam INCR8    = 3'b101;
-    localparam WRAP16   = 3'b110;
-    localparam INCR16   = 3'b111;
+    localparam [2:0] SINGLE     = 3'b000;
+    localparam [2:0] INCR       = 3'b001;
+    localparam [2:0] WRAP4      = 3'b010;
+    localparam [2:0] INCR4      = 3'b011;
+    localparam [2:0] WRAP8      = 3'b100;
+    localparam [2:0] INCR8      = 3'b101;
+    localparam [2:0] WRAP16     = 3'b110;
+    localparam [2:0] INCR16     = 3'b111;
 
+    //AHB SIZES
+    localparam [2:0] BYTE       = 3'b000;   // 8bits
+    localparam [2:0] HALFWORD   = 3'b001;   // 16bits
+    localparam [2:0] WORD       = 3'b010;   // 32bits
 
 
 
