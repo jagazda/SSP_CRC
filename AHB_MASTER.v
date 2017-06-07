4900a67418635eb01c06708070f05021389e1fb8
creex9 <gpcreex@gmail.com>
2017-06-07 23:34:27 +0200

    Added signals descriptions

--- AHB_MASTER.v
+++ AHB_MASTER.v
@@ -4,9 +4,12 @@ module AHB_MAST #(parameter [31:0] DATA_WIDTH = 32'd32,ADDR_WIDTH = 32'd32)(
     input wire HCLK,
     input wire RESET,
     //AMBA AHB SIGNALS
+    //Signals from SLAVE
     input wire [DATA_WIDTH-1:0] HRDATA,
     input wire HREADY,
     input wire HRESP,
+
+    //MASTER OUT
     output reg [ADDR_WIDTH-1:0] HADDR,
     output reg [DATA_WIDTH-1:0] HWDATA,
     output reg [2:0] HBURST,
@@ -17,5 +20,22 @@ module AHB_MAST #(parameter [31:0] DATA_WIDTH = 32'd32,ADDR_WIDTH = 32'd32)(
     output reg HSEL,
     
 );
+    //HTRANS TYPES
+    localparam IDLE     = 2'b00;
+    localparam BUSY     = 2'b01;
+    localparam NONSEQ   = 2'b10;
+    localparam SEQ      = 2'b11;
 
-    
+    //HBURST TYPES
+    localparam SINGLE   = 3'b000;
+    localparam INCR     = 3'b001;
+    localparam WRAP4    = 3'b010;
+    localparam INCR4    = 3'b011;
+    localparam WRAP8    = 3'b100;
+    localparam INCR8    = 3'b101;
+    localparam WRAP16   = 3'b110;
+    localparam INCR16   = 3'b111;
+
+
+
+
